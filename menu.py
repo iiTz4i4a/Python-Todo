c3ee0f8e1fd6961f27ff145b8b88217170f732e6
iiTz4i4a <cickalenkowork@outlook.com>
2026-08-20 13:30:40 +0200

    Project - curently adding some necessary files to the project

---
+++ menu.py
@@ -0,0 +1,8 @@
+from colors import GREEN, BOLD, RESET
+
+
+def start_menu():
+    userActions = [""]
+    print(f"{GREEN}{BOLD}---Todo Application---{RESET}")
+    for actions in userActions:
+        print(actions)
